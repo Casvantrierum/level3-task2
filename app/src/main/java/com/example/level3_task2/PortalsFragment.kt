1a9f7b310081be83dc3ffa9657f7ca0d5d86149c
Cas <Casvantrierum@gmail.com>
2020-09-22 22:57:44 +0200

    open webview working

--- app/src/main/java/com/example/level3_task2/PortalsFragment.kt
+++ app/src/main/java/com/example/level3_task2/PortalsFragment.kt
@@ -1,6 +1,8 @@
 package com.example.level3_task2
 
+import android.content.ComponentName
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -9,13 +11,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.browser.customtabs.CustomTabsSession
 import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.*
 import kotlinx.android.synthetic.main.fragment_portals.*
-import android.app.PendingIntent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,6 +27,11 @@ class PortalsFragment : Fragment() {
 
     private val portals = arrayListOf<Portal>()
     private val portalAdapter = PortalAdapter(portals, { portal : Portal -> portalClicked(portal) })
+
+    val CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
+    private var mCustomTabsServiceConnection: CustomTabsServiceConnection? = null
+    private var mClient: CustomTabsClient? = null
+    private var mCustomTabsSession: CustomTabsSession? = null
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +43,21 @@ class PortalsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mCustomTabsServiceConnection = object : CustomTabsServiceConnection() {
+            override fun onCustomTabsServiceConnected(componentName: ComponentName, customTabsClient: CustomTabsClient) {
+                //Pre-warming
+                mClient = customTabsClient
+                mClient?.warmup(0L)
+                mCustomTabsSession = mClient?.newSession(null)
+            }
+
+            override fun onServiceDisconnected(name: ComponentName) {
+                mClient = null
+            }
+        }
+        CustomTabsClient.bindCustomTabsService(context, CUSTOM_TAB_PACKAGE_NAME, mCustomTabsServiceConnection);
+
         initViews()
     }
 
@@ -60,5 +83,10 @@ class PortalsFragment : Fragment() {
 
     private fun portalClicked(portal : Portal) {
         Toast.makeText(context, portal.portalName, Toast.LENGTH_LONG).show()
+        val customTabsIntent = CustomTabsIntent.Builder(mCustomTabsSession)
+            .setShowTitle(true)
+            .build()
+
+        customTabsIntent.launchUrl(context, Uri.parse(portal.portalUrl))
     }
 }
